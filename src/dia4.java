3e2201c0e35b074caa5a7863abb9746c69894ea3
Carlos Torres <escolta87@hotmail.com>
2025-01-23 16:48:12 -0500

    creacion dia 4

---
+++ src/dia4.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class dia4 {
+    public static void main(String[] args) {
+        //Aqui desarrollaremos el dia 4 el cual consiste en el entendimiento de los metodos en java
+    }
+}
